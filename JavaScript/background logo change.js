c485682ff0efb86fea021dc90e273ff30f0de175
Berwin Singh <63018717+berwinsingh@users.noreply.github.com>
2023-09-02 12:04:01 +0530

    Added higher quality images

--- JavaScript/background logo change.js
+++ JavaScript/background logo change.js
@@ -1,4 +1,5 @@
-const weatherImg = ["./Images/Rainy.png","./Images/Sunny Day.png","../Images/Autumn.png"];
+const body = document.body;
+const weatherImg = ["../Images/Rainy.jpg","../Images/Sunny Day.jpg","../Images/autumn.jpg", "../Images/partly cloudy.jpg"];
 
 const weatherLogo = document.getElementById("weather-logo");
 const conditionIcons = {
@@ -10,8 +11,20 @@ const conditionIcons = {
 
 //This function checks the weather condition and applies icon based on that
 export default function checkCondition(weather){
-    if (weather === "Clear") weatherLogo.setAttribute("name", conditionIcons.clear);
-    if (weather === "Rainy") weatherLogo.setAttribute ("name",conditionIcons.rainy);
-    if (weather === "Sunny") weatherLogo.setAttribute ("name",conditionIcons.sunny);
-    if (weather === "Partly cloudy") weatherLogo.setAttribute ("name",conditionIcons.partlycloudy);
+    if (weather === "Clear"){
+        weatherLogo.setAttribute("name", conditionIcons.clear);
+        body.style.backgroundImage = `url("${weatherImg[2]}")`;
+    } 
+    if (weather === "Rainy") {
+        weatherLogo.setAttribute ("name",conditionIcons.rainy);
+        body.style.backgroundImage = `url("${weatherImg[0]})`;
+    }
+    if (weather === "Sunny") {
+        weatherLogo.setAttribute ("name",conditionIcons.sunny);
+        body.style.backgroundImage = `url("${weatherImg[1]})`;
+    }
+    if (weather === "Partly cloudy") {
+        weatherLogo.setAttribute ("name",conditionIcons.partlycloudy);
+        body.style.backgroundImage = `url("${weatherImg[3]}")`;
+    } 
 }
--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,7 +2,6 @@ import checkCondition from "./background logo change.js";
 
 const degreeSign = "°";
 
-
 const temperature = document.getElementById("temperature");
 const locationName = document.getElementById("location");
 const localTime = document.getElementById("time");
@@ -12,19 +11,19 @@ const weatherType = document.getElementById("weather-type");
 
 
 const weatherAPI = "fe88d13146604bba9c871846230109";
-const getLocation = "London"
+const getLocation = "Sydney"
 
 async function currentWeatherData (){
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${weatherAPI}&q=${getLocation}`,{mode:"cors"});
     const currentWeather = await response.json();
-    console.log(currentWeather);
+    // console.log(currentWeather);
 
     //Extracting values from the JSON received after pinging the API server
     const weatherInCelcius = currentWeather.current.feelslike_c;
     const location = currentWeather.location.name;
     const time = currentWeather.location.localtime;
     const dateTime = time.split(" ");
-    const currentCondition = currentWeather.current.condition.text
+    const currentCondition = currentWeather.current.condition.text;
     
     //Assigning values extracted to the DOM
     temperature.textContent= `${weatherInCelcius}${degreeSign}`;
